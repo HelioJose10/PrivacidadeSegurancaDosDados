06cfe964a4d7f9a11a49502d95dba73214e20a52
Hélio José <heliodivaldo2@hotmail.com>
2024-10-04 01:14:13 +0100

    4

--- P2PSecureChat/src/main/java/security/encryptionUtils.java
+++ P2PSecureChat/src/main/java/security/encryptionUtils.java
@@ -1,39 +1,25 @@
 package main.java.security;
 
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
 
 public class EncryptionUtils {
-    private static final String ALGORITHM = "AES";
-    private static final String CHARSET = "UTF-8";
 
-    public static String encrypt(String message, String secretKey) {
-        try {
-            SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes(), ALGORITHM);
-            Cipher cipher = Cipher.getInstance(ALGORITHM);
-            cipher.init(Cipher.ENCRYPT_MODE, keySpec);
-
-            byte[] encryptedBytes = cipher.doFinal(message.getBytes(CHARSET));
-            return Base64.getEncoder().encodeToString(encryptedBytes);
-        } catch (Exception e) {
-            throw new RuntimeException("Erro ao criptografar a mensagem", e);
-        }
+    public static String encrypt(String data, String key) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encryptedData = cipher.doFinal(data.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedData);  // Codificar em Base64
     }
 
-    public static String decrypt(String encryptedMessage, String secretKey) {
-        try {
-            SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes(), ALGORITHM);
-            Cipher cipher = Cipher.getInstance(ALGORITHM);
-            cipher.init(Cipher.DECRYPT_MODE, keySpec);
-
-            byte[] decodedBytes = Base64.getDecoder().decode(encryptedMessage);
-            byte[] decryptedBytes = cipher.doFinal(decodedBytes);
-            return new String(decryptedBytes, CHARSET);
-        } catch (Exception e) {
-            throw new RuntimeException("Erro ao descriptografar a mensagem", e);
-        }
+    public static String decrypt(String encryptedData, String key) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decodedData = Base64.getDecoder().decode(encryptedData);  // Decodificar de Base64
+        byte[] decryptedData = cipher.doFinal(decodedData);
+        return new String(decryptedData);
     }
 }
